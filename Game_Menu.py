bd701133dc8bfdc0a26ae047141da09e518027b8
Michelle Mu <michellemjx@gmail.com>
2024-04-27 09:57:08 -0700

    Small Tweaks to Game Menu - added time buffer to game menu when game loads - added print statement to display what game was chosen in game menu

--- Game_Menu.py
+++ Game_Menu.py
@@ -1,11 +1,13 @@
 """GAME MENU"""
 
 #DONE at the end of the game, if they chose not to play again, return to game menu
-#TODO add time buffer and 'game loading...' to after game is chosen
-#TODO print f string you have chosen X game
+#DONE add time buffer and 'game loading...' to after game is chosen
+#DONE print f string you have chosen X game
 
 from RockPaperScissors import main as rps_main
 from Blackjack import main as bj_main
+from termcolor import cprint
+import time
 
 menu_options = [
     'Exit',
@@ -28,6 +30,11 @@ def main():
         game = choice_of_game()
         if game is None:
             return
+        cprint('Loading your game', 'dark_grey', end='')
+        for _i in range(7):
+            cprint('.', 'dark_grey', end='', flush=True)
+            time.sleep(0.5)
+        print()
         game()
 
 
@@ -38,9 +45,9 @@ def choice_of_game():
         return None
     idx = int(game_choice)
     game_choice = menu_options[idx]
+    print(f'You have chosen to play {game_choice}')
 
     if game_choice == 'Rock, Paper, Scissors': 
-        # print(f'You have chosen {}')
         return rps_main
     elif game_choice == 'Blackjack':
         return bj_main
